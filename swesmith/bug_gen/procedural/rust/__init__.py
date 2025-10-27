77b70c070287f4ca6fff33c01e96e37eebf9351d
Kevin Li <kevinli020508@gmail.com>
2025-10-27 11:42:11 -0700

    Reweight the modifier likelihoods for Rust to boost bug candidates

--- swesmith/bug_gen/procedural/rust/__init__.py
+++ swesmith/bug_gen/procedural/rust/__init__.py
@@ -17,14 +17,14 @@ from swesmith.bug_gen.procedural.rust.remove import (
 )
 
 MODIFIERS_RUST: list[ProceduralModifier] = [
-    ControlIfElseInvertModifier(likelihood=0.75),
-    ControlShuffleLinesModifier(likelihood=0.75),
-    RemoveAssignModifier(likelihood=0.25),
-    RemoveConditionalModifier(likelihood=0.25),
-    RemoveLoopModifier(likelihood=0.25),
-    OperationBreakChainsModifier(likelihood=0.4),
-    OperationChangeConstantsModifier(likelihood=0.4),
-    OperationChangeModifier(likelihood=0.4),
-    OperationFlipOperatorModifier(likelihood=0.4),
-    OperationSwapOperandsModifier(likelihood=0.4),
+    ControlIfElseInvertModifier(likelihood=0.9),
+    ControlShuffleLinesModifier(likelihood=0.1),
+    RemoveAssignModifier(likelihood=0.1),
+    RemoveConditionalModifier(likelihood=0.1),
+    RemoveLoopModifier(likelihood=0.1),
+    OperationBreakChainsModifier(likelihood=0.9),
+    OperationChangeConstantsModifier(likelihood=0.9),
+    OperationChangeModifier(likelihood=0.9),
+    OperationFlipOperatorModifier(likelihood=0.9),
+    OperationSwapOperandsModifier(likelihood=0.9),
 ]
