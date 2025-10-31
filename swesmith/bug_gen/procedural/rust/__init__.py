5ab41799085283f46a4132f7815cf484e4032855
Kevin Li <kevinli020508@gmail.com>
2025-10-31 03:58:33 +0000

    Set modifier likelihood to 0.25 for Rust for fairness

--- swesmith/bug_gen/procedural/rust/__init__.py
+++ swesmith/bug_gen/procedural/rust/__init__.py
@@ -17,14 +17,14 @@ from swesmith.bug_gen.procedural.rust.remove import (
 )
 
 MODIFIERS_RUST: list[ProceduralModifier] = [
-    ControlIfElseInvertModifier(likelihood=0.9),
-    ControlShuffleLinesModifier(likelihood=0.1),
-    RemoveAssignModifier(likelihood=0.1),
-    RemoveConditionalModifier(likelihood=0.1),
-    RemoveLoopModifier(likelihood=0.1),
-    OperationBreakChainsModifier(likelihood=0.9),
-    OperationChangeConstantsModifier(likelihood=0.9),
-    OperationChangeModifier(likelihood=0.9),
-    OperationFlipOperatorModifier(likelihood=0.9),
-    OperationSwapOperandsModifier(likelihood=0.9),
+    ControlIfElseInvertModifier(likelihood=0.25),
+    ControlShuffleLinesModifier(likelihood=0.25),
+    RemoveAssignModifier(likelihood=0.25),
+    RemoveConditionalModifier(likelihood=0.25),
+    RemoveLoopModifier(likelihood=0.25),
+    OperationBreakChainsModifier(likelihood=0.25),
+    OperationChangeConstantsModifier(likelihood=0.25),
+    OperationChangeModifier(likelihood=0.25),
+    OperationFlipOperatorModifier(likelihood=0.25),
+    OperationSwapOperandsModifier(likelihood=0.25),
 ]
