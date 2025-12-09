499489e3b00a9afe5ff506991be0bf62b7f5601d
Kevin Li <kevinli020508@gmail.com>
2025-12-09 01:46:43 +0000

    Replace model with base Qwen2.5 coder

--- agent/infer.sh
+++ agent/infer.sh
@@ -1,1 +1,1 @@
-time uv run sweagent run-batch --config agent/swesmith_infer.yaml >> swebench_multilingual_rust_swe_smith_agent.log 2>&1
+time uv run sweagent run-batch --config agent/swesmith_infer.yaml >> swebench_multilingual_rust_qwen2.5-coder-32b-instruct.log 2>&1
