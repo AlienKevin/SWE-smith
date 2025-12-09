38ceab29eac768dc7c3b34161874b32617abaf63
Kevin Li <kevinli020508@gmail.com>
2025-12-09 02:11:55 +0000

    Warmup vllm server with one quick request in infer.sh

--- agent/infer.sh
+++ agent/infer.sh
@@ -1,1 +1,10 @@
+curl https://swe-b--example-vllm-inference-serve.modal.run/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "llm",
+    "messages": [
+      { "role": "user", "content": "Hi!" }
+    ]
+  }'
+
 time uv run sweagent run-batch --config agent/swesmith_infer.yaml >> swebench_multilingual_rust_qwen2.5-coder-32b-instruct.log 2>&1
