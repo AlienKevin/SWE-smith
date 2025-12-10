2f95b04c2adda24a4f06f003184d037f149dcad9
Kevin Li <kevinli020508@gmail.com>
2025-12-10 05:10:57 +0000

    Test with gpt-5-mini distilled model

--- agent/eval.sh
+++ agent/eval.sh
@@ -12,8 +12,8 @@ source .venv/bin/activate
 
 uv run python -m swebench.harness.run_evaluation \
     --dataset_name AlienKevin/SWE-bench_Multilingual \
-    --predictions_path ../SWE-agent/trajectories/ubuntu/swesmith_infer__openai--SWE-agent-LM-32B-Rust-268__t-0.00__p-1.00__c-0.00___swe_bench_multilingual_rust/preds.json \
+    --predictions_path SWE-agent/trajectories/ubuntu/swesmith_infer__openai--SWE-agent-LM-32B-Rust-gpt-91-epoch-0__t-0.00__p-1.00__c-0.00___swe_bench_multilingual_rust/preds.json \
     --split rust \
     --max_workers 50 \
-    --run_id multilingual_rust_eval_swe_agent_lm_32b_rust_268 \
-    --modal true >> eval_swebench_multilingual_rust_swe_agent_lm_32b_rust_268.log 2>&1
+    --run_id multilingual_rust_eval_swe_agent_lm_32b_rust_gpt_91_epoch_0 \
+    --modal true >> eval_swebench_multilingual_rust_swe_agent_lm_32b_rust_gpt_91_epoch_0.log 2>&1
